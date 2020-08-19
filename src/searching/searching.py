a662feacc4f4ae8844e626fdd4ffca9742e985e6
Joe V <joeveverka89@gmail.com>
2020-08-19 18:21:19 -0400

    MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,52 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+	# base
+	if end < start:
+		return -1 
 
+	mid = (end + start) // 2 
 
-# STRETCH: implement an order-agnostic binary search
-# This version of binary search should correctly find 
-# the target regardless of whether the input array is
-# sorted in ascending order or in descending order
-# You can implement this function either recursively 
-# or iteratively
+	#got it
+	if arr[mid] == target:
+		return mid
+
+	#target < mid, chekc left
+	elif arr[mid] > target:
+		return binary_search(arr, target, start, mid - 1)
+	else:
+		return binary_search(arr, target, mid + 1, end)
+
+def descending_binary_search(arr, target, start, end):
+    	# base
+    if end < start:
+    	return -1
+
+    mid = (end + start) // 2
+
+    # found 
+    if arr[mid] == target: 
+        return mid 
+
+        # target smaller than mid, check right
+    elif arr[mid] > target:
+        return descending_binary_search(arr, target, mid + 1, end)
+        # larger, check left
+    else:
+        return descending_binary_search(arr, target, start, mid - 1)            
+
+# 
+ 
 def agnostic_binary_search(arr, target):
-    # Your code here
+    
+    #check if descending
+    descending = arr[0] > arr[1]
+    if descending:
+        return descending_binary_search(arr, target, 0, len(arr)-1)
+    else:
+        return binary_search(arr, target, 0, len(arr)-1)
 
+
+arr1 = [-1,-2,-3,-4,-5,-6,-7]
+arr2 = [-7,-6,-5,-4,-3,-2,-1]
+result = agnostic_binary_search(arr2, -2)
+print(result)
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,19 +1,60 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
+import math
+
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
 
-    # Your code here
+    #adjusted the original variables
 
+    merged_arr = []
 
+    # MARK: counts
+    left_count = len(arrA)
+    right_count = len(arrB)
+    left_i, right_i = 0, 0  
+
+    # fill the merged_arr with sorted values, checking both arrays until one of them has been
+    # completely processed
+    print(f"array A: {arrA}")
+    print(f"array B: {arrB}")
+
+    while left_i < left_count and right_i < right_count:
+        if arrA[left_i] < arrB[right_i]:
+            merged_arr.append(arrA[left_i])
+            print(f"left: {arrA[left_i]}")
+            left_i += 1            
+        elif arrA[left_i] == arrB[right_i]:
+            left_i += 1
+            right_i += 1
+            merged_arr.append(arrA[left_i])
+            merged_arr.append(arrB[right_i])
+            print(f"both: {arrB[right_i]}")
+        else:
+            print(f"right: {arrB[right_i]}")
+            merged_arr.append(arrB[right_i])
+            right_i += 1            
+    # fill any remaining elements since the above loop will end when either array is processed - then return the result    
+    merged_arr += arrA[left_i:]
+    merged_arr += arrB[right_i:]
+    print(f"merged: {merged_arr}\n")
     return merged_arr
-
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
 
+    # base case
+    if len(arr) <= 1:
 
-    return arr
+        return arr
+
+    # find the midpoint
+    mid = len(arr)//2
+
+    # split and recursively sort and merge the result
+    left =  merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+        
+    return merge(left, right)
+
+arr1 = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print(f"final: {merge_sort(arr1)}")
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +62,11 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
-
+    pass
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    pass
+
+
 
